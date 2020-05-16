811d42fffe3a98cd47bd3c94e154fee0b9bc367c
Pablo Valenzuela <p.valenzuela17@gmail.com>
2020-05-16 15:27:54 -0500

    still working

--- client.js
+++ client.js
@@ -1,1 +1,28 @@
-console.log("CALCULATOR")
+$(document).ready(function () {
+    $('#submitButton').on('click', function () {
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let idNumber = $('#idNumber').val();
+        let jobTitle = $('#jobTitle').val();
+        let annualSalary = $('#annualSalary').val();
+
+        $('#employeeTable').append('<tr>' +
+            '<td>' + firstName + '</td>' +
+            '<td>' + lastName + '</td>' +
+            '<td>' + idNumber + '</td>' +
+            '<td>' + jobTitle + '</td>' +
+            '<td>' + annualSalary + '</td>' +
+            '</tr>');
+
+        let monthlySalaryExpenditures = annualSalary / 12;
+        let previousSalaryTotal = $('#monthlyExpenses').text();
+        let totalMonthlyExpenses = parseFloat(previousSalaryTotal) + monthlySalaryExpenditures;
+        $('#monthlyExpenses').text(totalMonthlyExpenses);
+
+        $('input').val('');
+    });
+});
+
+
+
+
